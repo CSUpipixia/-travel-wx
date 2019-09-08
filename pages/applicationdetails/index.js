85505b01e8319550bd6a68a6540bc38e0e7b83b3
CSUpipixia <CSUpipixia@users.noreply.github.com>
2019-09-08 22:40:12 +0800

    增加申请详细信息和上传凭证页面

--- pages/applicationdetails/index.js
+++ pages/applicationdetails/index.js
@@ -1,66 +1,23 @@
-// pages/applicationdetails/index.js
+const app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    DetailData: {
+      'applicant_id': '',
+      'startdate': '2018-12-25',
+      'duration': '1',
+      'start_site': ['湖南省', '长沙市', '岳麓区'],
+      'destination': ['湖南省', '长沙市', '岳麓区'],
+      'vehicle': '飞机',
+      'business_reason': '',
+      'remark': ''
+    },
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 查看申请表详细信息
+  toRefundTap: function(e) {
+    wx.navigateTo({
+      url: "/pages/refund/refund"
+    })
   }
 })
