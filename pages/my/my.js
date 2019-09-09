605ef58f20eca5e76060eea90d4113236955ae04
CSUpipixia <CSUpipixia@users.noreply.github.com>
2019-09-09 08:11:25 +0800

    更新标题样式

--- pages/my/my.js
+++ pages/my/my.js
@@ -3,71 +3,59 @@ const CONFIG = require('../../config.js')
 const WXAPI = require('../../wxapi/router')
 const AUTH = require('../../utils/auth')
 
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+Component({
+  options: {
+    addGlobalClass: true,
+  },
   data: {
     userInfo: {
       nickName: "薛涛",
       avatarUrl: "../../image/nologin.png"
+    },
+    allTimes: 0,
+    allCitys: 0,
+    allRefunds: 0
+  },
+  attached() {
+    console.log("success")
+    let that = this;
+    wx.showLoading({
+      title: '数据加载中',
+      mask: true,
+    })
+    let i = 0;
+    numDH();
+    function numDH() {
+      if (i < 20) {
+        setTimeout(function () {
+          that.setData({
+            allTimes: i,
+            allCitys: i,
+            allRefunds: i
+          })
+          i++
+          numDH();
+        }, 20)
+      } else {
+        that.setData({
+          allTimes: that.coutNum(200),
+          allCitys: that.coutNum(98),
+          allRefunds: that.coutNum(24000)
+        })
+      }
     }
+    wx.hideLoading()
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  methods: {
+    // 数字转换
+    coutNum(e) {
+      if (e > 1000 && e < 10000) {
+        e = (e / 1000).toFixed(1) + 'k'
+      }
+      if (e > 10000) {
+        e = (e / 10000).toFixed(1) + 'W'
+      }
+      return e
+    },
   }
 })
